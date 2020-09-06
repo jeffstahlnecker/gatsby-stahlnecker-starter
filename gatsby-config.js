bad37fc857232f34300166b684a110b4abca9d44
stahldom <jeff@stahlnecker.me>
2020-09-06 11:53:58 +0200

    Switch styled components for tailwind with emotion

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-emotion`,
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
